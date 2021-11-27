2af5cfbb09962f623c7f00f0701b1ba363266811
jaa2 <43010335+jaa2@users.noreply.github.com>
2021-11-27 01:11:10 -0600

    Add linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-function App() {
+const App = function App() {
   return (
     <div>
       <h2>Nussinov</h2>
-      Our app's content will go here.
+      Our app&apos;s content will go here.
     </div>
   );
-}
+};
 
 export default App;
