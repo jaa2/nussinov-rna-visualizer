64f213cd88d0d136c338f3e952236ce126b92c56
jaa2 <43010335+jaa2@users.noreply.github.com>
2021-11-26 23:37:12 -0600

    Initialize repo with basic TypeScript React app

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('Nussinov text', () => {
+  render(<App />);
+  const element = screen.getByText(/Nussinov/i);
+  expect(element).toBeInTheDocument();
+});
