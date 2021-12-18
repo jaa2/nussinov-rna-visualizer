39e38d1b6f7a97a3058dda9e57012eda9dccfa83
jaa2 <43010335+jaa2@users.noreply.github.com>
2021-12-17 22:25:00 -0600

    Integrate Nussinov function and input checking

--- src/cleanFastaFile.ts
+++ src/cleanFastaFile.ts
@@ -29,18 +29,23 @@ export function sanitizeRNAString(data: string): [string, string | null] {
  * @returns an array of the potential errors of the passed RNA string
  */
 export function bioCheck(data: string): string[] {
+  if (data.length === 0) {
+    return [];
+  }
   const messages: string[] = [];
   if (data.length % 3 !== 0) {
-    messages.push('The RNA sequence is not divisable by 3; therefore the string has an abiguous protein.');
+    messages.push('The RNA sequence is not divisible by 3; the string has an ambiguous protein.');
   }
-  let end = '';
-  end = end.concat(
-    (data[data.length - 3]).toString(),
-    (data[data.length - 2]).toString(),
-    (data[data.length - 1]).toString(),
-  );
-  if (!(end === 'UAA' || end === 'UAG' || end === 'UGA')) {
-    messages.push('The RNA sequence does not contain a proper end sequence.');
+  if (data.length >= 3) {
+    let end = '';
+    end = end.concat(
+      (data[data.length - 3]).toString(),
+      (data[data.length - 2]).toString(),
+      (data[data.length - 1]).toString(),
+    );
+    if (!(end === 'UAA' || end === 'UAG' || end === 'UGA')) {
+      messages.push('The RNA sequence does not contain a proper end sequence.');
+    }
   }
   return (messages);
 }
