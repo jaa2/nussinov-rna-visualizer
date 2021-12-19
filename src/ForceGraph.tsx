14afa1c9f1a222e0ce90d34ea7a782ec88ad6d8f
Seth Wyma <wyma2@illinois.edu>
2021-12-18 23:05:41 -0600

    Removed unnecessary ForceGraph inputs

--- src/ForceGraph.tsx
+++ src/ForceGraph.tsx
@@ -5,62 +5,34 @@ import { INussinovPlotProps } from './NussinovPlot';
 // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
 // https://observablehq.com/@d3/force-directed-graph
-function drawForceGraph({
-  nodes, // an iterable of node objects (typically [{id}, …])
-  links, // an iterable of link objects (typically [{source, target}, …])
-}: any, {
-  nodeId = (d: any) => d.id, // given d in nodes, returns a unique identifier (string)
-  nodeGroup, // given d in nodes, returns an (ordinal) value for color
-  nodeGroups, // an array of ordinal values representing the node groups
-  nodeFill = 'currentColor', // node stroke fill (if not using a group color encoding)
-  nodeStroke = '#fff', // node stroke color
-  nodeStrokeWidth = 1.5, // node stroke width, in pixels
-  nodeStrokeOpacity = 1, // node stroke opacity
-  nodeRadius = 5, // node radius, in pixels
-  nodeStrength,
-  linkSource = ({ source }: any) => source, // given d in links, returns a node identifier string
-  linkTarget = ({ target }: any) => target, // given d in links, returns a node identifier string
-  linkStroke = '#999', // link stroke color
-  linkStrokeOpacity = 0.6, // link stroke opacity
-  linkStrokeWidth = 1.5, // given d in links, returns a stroke width in pixels
-  linkStrokeLinecap = 'round', // link stroke linecap
-  colors = d3.schemeTableau10, // an array of color strings, for the node groups
-  width = 800, // outer width, in pixels
-  height = 600, // outer height, in pixels
-  invalidation, // when this promise resolves, stop the simulation
-}: any = {}) {
-  // Compute values.
+function drawForceGraph(
+  { nodes, links }: any,
+  {
+    width = 800,
+    height = 600,
+    windowBuffer = 30,
+    linkColor = '#999',
+    linkOpacity = 0.6,
+    nodeRadiusPixels = 5,
+    nodeColor = '#999',
+  }: any = { },
+) {
   function intern(value: any) {
     return value !== null && typeof value === 'object' ? value.valueOf() : value;
   }
 
-  const N = d3.map(nodes, nodeId).map(intern);
-  const LS = d3.map(links, linkSource).map(intern);
-  const LT = d3.map(links, linkTarget).map(intern);
-  // const G = nodeGroup == null ? null : d3.map(nodes, nodeGroup).map(intern);
-  // const W = typeof linkStrokeWidth !== 'function' ? null : d3.map(links, linkStrokeWidth);
+  const N = d3.map(nodes, (d: any) => d.id).map(intern);
+  const LS = d3.map(links, ({ source }: any) => source).map(intern);
+  const LT = d3.map(links, ({ target }: any) => target).map(intern);
 
-  // Replace the input nodes and links with mutable objects for the simulation.
   nodes = d3.map(nodes, (_, i) => ({ id: N[i] })); // eslint-disable-line
   links = d3.map(links, (_, i) => ({ source: LS[i], target: LT[i] })); // eslint-disable-line
-
-  // Compute default domains.
-  // if (G && nodeGroups === undefined) nodeGroups = d3.sort(G);
-
-  // Construct the scales.
-  const color = nodeGroup == null ? null : d3.scaleOrdinal(nodeGroups, colors);
 
   // Construct the forces.
   const forceNode = d3.forceManyBody();
   const forceLink = d3.forceLink(links).id(({ index: i }: any) => N[i]);
-  if (nodeStrength !== undefined) forceNode.strength(nodeStrength);
 
-  forceNode.strength((node: any): number => {
-    if (node.index >= 9 && node.index <= 11) {
-      return -5;
-    }
-    return -5;
-  });
+  forceNode.strength(-5);
 
   forceLink.strength((link: any): number => {
     if (link.target.index - link.source.index === 1) {
@@ -97,6 +69,10 @@ function drawForceGraph({
         .attr('cx', (d: any) => d.x)
         .attr('cy', (d: any) => d.y);
 
+      bpText // eslint-disable-line
+        .attr('x', (d: any) => d.x)
+        .attr('y', (d: any) => d.y);
+
       if (nodes.length > 0) {
         let window = [nodes[0].x, nodes[0].y, nodes[0].x, nodes[0].y];
         for (let i = 1; i < nodes.length; i += 1) {
@@ -104,35 +80,32 @@ function drawForceGraph({
             Math.max(window[2], nodes[i].x), Math.max(window[3], nodes[i].y)];
         }
 
-        const buffer = 30;
-
         svg
-          .attr('viewBox', [window[0] - buffer,
-            window[1] - buffer,
-            window[2] - window[0] + 2 * buffer,
-            window[3] - window[1] + 2 * buffer]);
+          .attr('viewBox', [window[0] - windowBuffer,
+            window[1] - windowBuffer,
+            window[2] - window[0] + 2 * windowBuffer,
+            window[3] - window[1] + 2 * windowBuffer]);
       }
     });
 
   const link = svg.append('g')
-    .attr('stroke', linkStroke)
-    .attr('stroke-opacity', linkStrokeOpacity)
-    .attr('stroke-width', typeof linkStrokeWidth !== 'function' ? linkStrokeWidth : null)
-    .attr('stroke-linecap', linkStrokeLinecap)
+    .attr('stroke', linkColor)
+    .attr('stroke-opacity', linkOpacity)
     .selectAll('line')
     .data(links)
     .join('line');
 
   const node = svg.append('g')
-    .attr('fill', nodeFill)
-    .attr('stroke', nodeStroke)
-    .attr('stroke-opacity', nodeStrokeOpacity)
-    .attr('stroke-width', nodeStrokeWidth)
+    .attr('stroke', nodeColor)
     .selectAll('circle')
     .data(nodes)
     .join('circle')
-    .attr('r', nodeRadius)
+    .attr('r', nodeRadiusPixels)
     .call(drag(simulation)); // eslint-disable-line
+
+  const bpText = node.append('title')
+    .style('fill', 'orange') // fill the text with the colour black
+    .text('Hello World'); // define the text to display
 
   function drag(sim: any): any {
   /* eslint-disable no-param-reassign */
@@ -159,14 +132,9 @@ function drawForceGraph({
       .on('end', dragended);
   }
 
-  // if (W) link.attr('stroke-width', ({index: i}) => W[i]);
-  // if (G) node.attr('fill', ({index: i}) => color(G[i]));
-  // if (T) node.append('title').text(({index: i}) => T[i]);
-  link.attr('stroke-width', linkStrokeWidth);
+  // link.attr('stroke-width', linkStrokeWidth);
 
-  if (invalidation != null) invalidation.then(() => simulation.stop());
-
-  return Object.assign(svg.node(), { scales: { color } });
+  return Object.assign(svg.node(), { });
 }
 
 interface Link {
